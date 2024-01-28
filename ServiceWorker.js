1ce73f3c360cba9bb191d4af9e7ea4f7460ac175
Amit <81366957+amitkr000@users.noreply.github.com>
2024-01-29 03:45:18 +0530

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Aimins-Cubills-0.1";
+const contentToCache = [
+    "Build/Mobile_Browser.loader.js",
+    "Build/Mobile_Browser.framework.js",
+    "Build/Mobile_Browser.data",
+    "Build/Mobile_Browser.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
